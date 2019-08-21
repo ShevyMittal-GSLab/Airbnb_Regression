9c0f81256ec601f24da187098add3249e159f586
Shevy Mittal <shevy.mittal@gslab.com>
2019-08-21 19:03:17 +0530

    Getting all spark properties

--- testFile.py
+++ testFile.py
@@ -2,9 +2,12 @@
 from pyspark.sql import SparkSession
 
 warehouse_location = "/apps/spark"
-spark = SparkSession.builder.getOrCreate
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+spark = SparkSession.builder.getOrCreate()
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(spark)
+print(spark.master)
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>2222")
+print(spark.sparkContext.getConf().getAll())
 df = spark.sql('show databases').toPandas()
 print(df)
 df2 =  spark.sql('show tables').toPandas()
