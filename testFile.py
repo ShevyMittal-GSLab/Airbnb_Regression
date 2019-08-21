f485740acef9abee1c40db30a855042e20e94977
Shevy Mittal <shevy.mittal@gslab.com>
2019-08-21 18:56:24 +0530

    Printing Spark configurations

--- testFile.py
+++ testFile.py
@@ -2,7 +2,8 @@
 from pyspark.sql import SparkSession
 
 warehouse_location = "/apps/spark"
-spark = SparkSession.builder.config("spark.sql.warehouse.dir", warehouse_location).getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+print(spark)
 df = spark.sql('show databases').toPandas()
 print(df)
 df2 =  spark.sql('show tables').toPandas()
