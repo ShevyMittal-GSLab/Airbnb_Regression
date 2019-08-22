4b5eb6004f1a8c5f756e47c1b1fb8958cabfc2dd
Shevy Mittal <shevy.mittal@gslab.com>
2019-08-22 14:22:01 +0530

    Changing back to testFile.py

--- testFile.py
+++ testFile.py
@@ -3,11 +3,10 @@ from pyspark.sql import SparkSession
 
 warehouse_location = "/apps/spark"
 spark = SparkSession.builder.config('spark.sql.catalogImplementation','hive').getOrCreate()
+df = spark.sql('select * from knime_datasets.queens')
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print(spark)
-print(spark.catalog)
+print(df)
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>2222")
-print(spark.sparkContext.getConf().getAll())
 df = spark.sql('show databases').toPandas()
 print(df)
 df2 =  spark.sql('show tables').toPandas()
