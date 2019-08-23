32267ac21d4c5f16fb1bf162a892497bd822c5d2
Shevy Mittal <shevy.mittal@gslab.com>
2019-08-23 16:41:15 +0530

    Changing test File

--- testFile.py
+++ testFile.py
@@ -2,9 +2,23 @@
 from pyspark.sql import SparkSession
 
 def testFile():
-	warehouse_location = "/apps/spark"
 	spark = SparkSession.builder.config('spark.sql.catalogImplementation','hive').getOrCreate()
-	df = spark.sql('select * from knime_datasets.queens')
+	df = spark.sql('select * from knime_datasets.queens').toPandas()
+	target = "Review_Scores_Rating5"
+	df = df[df.Review_Scores_Rating5.notnull()]
+	df = df[df.Number_of_Records.notnull()]
+	df = df[df.Number_Of_Reviews.notnull()]
+	df = df[df.Review_Scores_Rating12.notnull()]
+	df['Review_Scores_Rating5'] = df.Review_Scores_Rating5.astype(float)
+	df['Number_of_Records'] = df.Number_of_Records.astype(float)
+	df['Number_Of_Reviews'] = df.Number_Of_Reviews.astype(float)
+	df['Review_Scores_Rating12'] = df.Review_Scores_Rating12.astype(float)
+	df['Review_Scores_Rating5'] = df.Review_Scores_Rating5.astype(float)
+	train, test = train_test_split(df)
+	train_x = train[['Number_of_Records', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
+	test_x = test[['Number_of_Records', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
+	train_y = train[["Review_Scores_Rating5"]]
+	test_y = test[["Review_Scores_Rating5"]]
 	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 	print(df)
 	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>2222")
